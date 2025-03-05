e8db1ede2852efb455132d17f06ded7ca7f0afe7
Mert Özbolat <mertozbolat008@gmail.com>
2025-03-05 15:13:06 +0300

    update

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 
 
+
 export const metadata = {
     title: "Login",
     description: "Giriş Yapınız",
 };
 
-const Login = () => {
+
+export const delay = async (ms = 1000) => {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+
+const Login = async () => {
+    await delay(2000)
+    throw new Error('Giriş Yetkini Yk')
     return (
         <div>Login</div>
     )
